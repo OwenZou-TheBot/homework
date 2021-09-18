84337d57bc3de0cc5d7ae1243d3d49604d66b12c
OwenZou-TheBot <79731673+OwenZou-TheBot@users.noreply.github.com>
2021-09-18 14:04:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import './App.css';
-import AddingNumbers from './components/AddingNumbers'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const pokemonsPerPage = 20;
+    const pokemonsToSkip = (page - 1) * pokemonsPerPage;
+    setLoading(true);
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${pokemonsPerPage}&offset=${pokemonsToSkip}`,
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data.results);
+        setLoading(false);
+      });
+  }, [page]);
   return (
     <div className='App'>
-      <AddingNumbers />
+      <div className='container'>
+        {data
+          ? data.map((v, i) => (
+              <div className='pokemon'>
+                <p>{v.name}</p>
+              </div>
+            ))
+          : 'Loading Pokemons'}
+      </div>
+      <div className='pagination'>
+        <button
+          disabled={page === 1 || loading}
+          onClick={() => setPage(page === 1 ? 1 : page - 1)}
+        >
+          Prev
+        </button>
+        <button disabled={loading} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+      <p className='page-number'>Page {page}</p>
     </div>
   );
 }
